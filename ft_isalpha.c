a2a09b5d8ffe3ce16746b4f87ea343f694be4112
Semere M. Mebrahtom <semere.meharena1@gmail.com>
2025-11-25 14:11:20 +0100

    Committing changes in the parent repo

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,65 +6,263 @@
 /*   By: semebrah <semebrah@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 13:34:16 by semebrah          #+#    #+#             */
-/*   Updated: 2025/11/24 15:38:47 by semebrah         ###   ########.fr       */
+/*   Updated: 2025/11/25 14:11:03 by semebrah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdlib.h>
+#include <stddef.h>
+#include <stdlib.h>
 
-int ft_isalpha(int c) {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+int	ft_islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
 }
 
-int ft_isdigit(int c) {
+int	ft_isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
+
+int	ft_isdigit(int c)
+{
 	return (c >= '0' && c <= '9');
 }
 
-int ft_isalnum(int c) {
+int	ft_isalpha(int c)
+{
+	return (ft_islower(c) || ft_isupper(c));
+}
+
+int	ft_isalnum(int c)
+{
 	return (ft_isalpha(c) || ft_isdigit(c));
 }
 
-int ft_isascii(int c) {
+int	ft_isascii(int c)
+{
 	return (c >= 0 && c < 128);
 }
 
-int ft_isprint(int c) {
+int	ft_isprint(int c)
+{
 	return (c > 32 && c < 127);
 }
 
-size_t ft_strlen(const char *s) {
-	size_t len = 0;
+int	ft_isspace(int c)
+{
+	return (c == ' ' || (c >= 9 && c <= 13));
+}
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
 	while (s[len])
 		len++;
-
-	return len;
+	return (len);
 }
 
-void *ft_memset(void *s, int c, size_t n){
-	size_t i = 0;
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	i;
 
+	i = 0;
 	while (i < n)
 		((char *)s)[i++] = c;
-
-	return s;
+	return (s);
 }
 
-void ft_bzero(void *s, size_t n) {
+void	ft_bzero(void *s, size_t n)
+{
 	ft_memset(s, 0, n);
 }
 
-void *ft_memcpy(void *dest, const void *src, size_t n){
-	size_t i = 0;
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
 
-	while (i < n){
+	i = 0;
+	while (i < n)
+	{
 		((char *)dest)[i] = ((char *)src)[i];
 		i++;
 	}
-	
-	return dest;
+	return (dest);
 }
 
-void *memmove(void *dest, const void *src, size_t n){
-		
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char	temp[n];
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((char *)temp)[i] = ((char *)src)[i];
+		i++;
+	}
+	i = 0;
+	while (i < n)
+	{
+		((char *)dest)[i] = ((char *)temp)[i];
+		i++;
+	}
+	return (dest);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < size - 1 && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (i);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	dst_len;
+	size_t	src_len;
+
+	i = 0;
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	while (src[i] && dst_len + i < size - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst_len + src_len);
+}
+
+int	ft_toupper(int c)
+{
+	if (ft_islower(c))
+		c -= 32;
+	return (c);
+}
+
+int	ft_tolower(int c)
+{
+	if (ft_isupper(c))
+		c += 32;
+	return (c);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (char *)(s + i);
+		i++;
+	}
+	if (s[i] == c)
+		return (char *)(s + i);
+	return (NULL);
+}
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	if (s[i] == c)
+		return (char *)(s + i);
+	while (c >= 0)
+	{
+		if (s[i] == c)
+			return (char *)(s + i);
+		i--;
+	}
+	return (NULL);
+}
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n - 1 && s1[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	l_len;
+
+	i = 0;
+	l_len = ft_strlen(little);
+	while (i < len - l_len)
+	{
+		if (ft_strncmp(&big[i], little, len))
+			return (char *)(big + i);
+		i++;
+	}
+	return (NULL);
+}
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*s_ptr;
+
+	i = 0;
+	s_ptr = (unsigned char *)s;
+	while (i < n && s_ptr[i] != c)
+		i++;
+	if (s_ptr[i] == c)
+		return (void *)(s + i);
+	return (NULL);
+}
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*s1_ptr;
+	unsigned char	*s2_ptr;
+
+	i = 0;
+	s1_ptr = (unsigned char *)s1;
+	s2_ptr = (unsigned char *)s2;
+	while (i < n - 1 && s1_ptr[i] == s2_ptr[i])
+		i++;
+	return (s1_ptr[i] - s2_ptr[i]);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	size_t	i;
+	int		res;
+	int		sign;
+
+	i = 0;
+	res = 0;
+	sign = 0;
+	while (ft_isspace(nptr[i++]))
+		;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (ft_isdigit(nptr[i]))
+		res = res * 10 + nptr[i++] - '0';
+	return (res * sign);
 }
