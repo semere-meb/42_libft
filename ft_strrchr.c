d9cb648bd7ce01677dc9c6b3736ca466b851413e
Semere M. Mebrahtom <semere.meharena1@gmail.com>
2025-11-26 16:28:32 +0100

    fix: corrected the while loop test

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -19,9 +19,7 @@ char	*ft_strrchr(const char *s, int c)
 	i = 0;
 	while (s[i])
 		i++;
-	if (s[i] == c)
-		return ((char *)(s + i));
-	while (c >= 0)
+	while (i >= 0)
 	{
 		if (s[i] == c)
 			return ((char *)(s + i));
