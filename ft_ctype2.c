27f6ccbf4c87f37a0e04546dd051c13352e1f253
Semere M. Mebrahtom <semere.meharena1@gmail.com>
2026-01-25 20:55:46 +0100

    organized all ctype functions together and added isspace function

--- ft_ctype2.c
+++ ft_ctype2.c
@@ -1,14 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*   ft_ctypes2.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: semebrah <semebrah@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/25 15:54:16 by semebrah          #+#    #+#             */
-/*   Updated: 2025/11/25 15:54:23 by semebrah         ###   ########.fr       */
+/*   Created: 2025/11/25 15:51:34 by semebrah          #+#    #+#             */
+/*   Updated: 2025/11/25 15:52:39 by semebrah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isascii(int c)
+{
+	return (c >= 0 && c < 128);
+}
+
+int	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
 
 int	ft_isprint(int c)
 {
--- libft.h
+++ libft.h
@@ -13,12 +13,12 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include "stdarg.h"
 # include <ctype.h>
-# include <stdlib.h>
-# include <unistd.h>
 # include <fcntl.h>
-#include <sys/select.h>
-#include "stdarg.h"
+# include <stdlib.h>
+# include <sys/select.h>
+# include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 100
@@ -36,25 +36,28 @@ typedef struct s_list
 
 typedef struct s_template
 {
-	size_t	start;
-	size_t	end;
+	size_t			start;
+	size_t			end;
 
-	size_t	len;
+	size_t			len;
 
-	int		left;
-	int		sign;
-	int		space;
-	int		alt;
-	int		zero;
-	int		width;
-	int		precision;
-	char	specifier;
-}			t_template;
+	int				left;
+	int				sign;
+	int				space;
+	int				alt;
+	int				zero;
+	int				width;
+	int				precision;
+	char			specifier;
+}					t_template;
 
-int					ft_isalpha(int c);
-int					ft_isdigit(int c);
+int					ft_islower(int c);
+int					ft_isupper(int c);
 int					ft_isalnum(int c);
+int					ft_isalpha(int c);
+int					ft_isspace(int c);
 int					ft_isascii(int c);
+int					ft_isdigit(int c);
 int					ft_isprint(int c);
 size_t				ft_strlen(const char *s);
 void				*ft_memset(void *s, int c, size_t n);
@@ -98,48 +101,50 @@ void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
 						void (*del)(void *));
 
-char	*get_next_line(int fd);
-ssize_t	get_index(const char *s, char c, size_t size);
-char	*append_buff(char *s1, char *s2, size_t size);
-void	shift_remainder(char *buff, size_t start, size_t size);
+char				*get_next_line(int fd);
+ssize_t				get_index(const char *s, char c, size_t size);
+char				*append_buff(char *s1, char *s2, size_t size);
+void				shift_remainder(char *buff, size_t start, size_t size);
 
 // ft_printf.c
-int			ft_printf(const char *f, ...);
+int					ft_printf(const char *f, ...);
 
 // parser.c
-void		ft_reset_template(t_template *tpl);
-int			ft_next_template(const char *str, size_t start, t_template *tpl);
+void				ft_reset_template(t_template *tpl);
+int					ft_next_template(const char *str, size_t start,
+						t_template *tpl);
 
 // converter.c
-char		*convert(unsigned long long n, char *base_to);
+char				*convert(unsigned long long n, char *base_to);
 
 // utils.c
-int			ft_is_member(char c, char *chars);
-ssize_t		ft_index(const char *s, size_t start, char *chars);
-char		*strgen(char c, size_t size);
-char		*append(char *s1, char *s2, ssize_t s1size, ssize_t s2size);
+int					ft_is_member(char c, char *chars);
+ssize_t				ft_index(const char *s, size_t start, char *chars);
+char				*strgen(char c, size_t size);
+char				*append(char *s1, char *s2, ssize_t s1size, ssize_t s2size);
 
 // printers1.c
-void		print_c(int c, t_template *tpl);
-void		print_s(char *str, t_template *tpl);
-void		print_p(unsigned long long val, t_template *tpl);
-void		print_d(long long val, t_template *tpl);
-void		print_u(unsigned int val, t_template *tpl);
+void				print_c(int c, t_template *tpl);
+void				print_s(char *str, t_template *tpl);
+void				print_p(unsigned long long val, t_template *tpl);
+void				print_d(long long val, t_template *tpl);
+void				print_u(unsigned int val, t_template *tpl);
 
 // printers2.c
-void		print_x(unsigned int val, t_template *tpl);
-void		print(char *str, t_template *tpl, size_t size);
+void				print_x(unsigned int val, t_template *tpl);
+void				print(char *str, t_template *tpl, size_t size);
 
 // handlers1.c
-char		*handle_precision(t_template *tpl, char *str, int zero);
-char		*handle_width(t_template *tpl, int size, char c, int negative);
-char		*handle_left(t_template *tpl, char *str, char *str_gen,
-				size_t str_size);
-char		*handle_alt(t_template *tpl, char *str);
-char		*handle_sign(t_template *tpl, char *str, int negative);
+char				*handle_precision(t_template *tpl, char *str, int zero);
+char				*handle_width(t_template *tpl, int size, char c,
+						int negative);
+char				*handle_left(t_template *tpl, char *str, char *str_gen,
+						size_t str_size);
+char				*handle_alt(t_template *tpl, char *str);
+char				*handle_sign(t_template *tpl, char *str, int negative);
 
 // handlers2.c
-char		*handle_space(t_template *tpl, char *str, int negative);
-char		handle_zero(t_template *tpl);
+char				*handle_space(t_template *tpl, char *str, int negative);
+char				handle_zero(t_template *tpl);
 
 #endif
